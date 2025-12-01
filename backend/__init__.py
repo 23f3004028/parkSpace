78aa410e944f4002b0ec0226da1f1b58b56e391e
23f3004028@WIN11 <23f3004028@ds.study.iitm.ac.in>
2025-12-01 13:01:04 +0530

    App factory initialised

---
+++ backend/__init__.py
@@ -0,0 +1,23 @@
+#app factory
+from flask import Flask
+from controllers.base_views import u_view
+from controllers.user_page import user_view
+from controllers.admin_page import admin_view
+from models.config import initialize_table
+from flask_cors import CORS
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.jinja_env.globals.update(int=int)
+    app.secret_key = 'dharshan'
+    CORS(app, resources={r"/*": {
+        "origins": ["http://localhost:5173", "http://127.0.0.1:5173"]
+    }}, supports_credentials=True)
+
+    initialize_table()
+    app.register_blueprint(u_view)
+    app.register_blueprint(user_view)
+    app.register_blueprint(admin_view)
+    return app
