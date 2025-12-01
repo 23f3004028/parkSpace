051b5a598d55b1cb3c308f770b998d5b59ec1545
23f3004028@WIN11 <23f3004028@ds.study.iitm.ac.in>
2025-12-01 12:32:49 +0530

    Initial commit

---
+++ backend/models/config.py
@@ -0,0 +1,23 @@
+import sqlite3
+from .database import create_database   #If i dont use .database python will look from working database and not relative
+from werkzeug.security import generate_password_hash
+
+def initialize_table():
+    create_database()
+    conn = sqlite3.connect("Parking.db")
+    curr = conn.cursor()
+
+    admin_id = 0
+    admin_username = "admin@admin.com"
+    admin_password = generate_password_hash("admin")
+    is_admin = True
+
+
+    curr.execute('''INSERT OR REPLACE INTO USERS(id,email,password,is_admin) VALUES(?,?,?,?);''',(admin_id,admin_username,admin_password,is_admin)) #insert admin cred into table and ignore if already exists
+
+    conn.commit()
+    conn.close()
+
+    #print("Admin credentials added into tabke /n")
+
+    
---
+++ backend/models/database.py
@@ -0,0 +1,61 @@
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect("Parking.db")       #connects and creates db if not available
+    conn.execute("PRAGMA foreign_keys = ON")  # Since our table has foreign keys it enables foreign keys
+    curr = conn.cursor()                       #cursor to execute queries
+
+    curr.execute('''
+                CREATE TABLE IF NOT EXISTS USERS(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT NOT NULL UNIQUE,               
+                name TEXT ,
+                address TEXT,
+                pincode INTEGER,
+                password TEXT NOT NULL,
+                is_admin BOOLEAN DEFAULT FALSE                  
+                );
+                ''')
+
+    curr.execute('''
+                CREATE  TABLE IF NOT EXISTS PARKING_LOT(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                prime_location TEXT NOT NULL UNIQUE,
+                price INTEGER ,
+                address TEXT NOT NULL,
+                pincode TEXT NOT NULL,
+                max_no_of_spots INTEGER NOT NULL,
+                no_of_available INTEGER
+                );
+                ''')
+    curr.execute('''CREATE TABLE IF NOT EXISTS PARKING_SPOT(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                lot_id INTEGER NOT NULL,
+                spot_number TEXT NOT NULL,
+                status TEXT DEFAULT 'A',
+                FOREIGN KEY(lot_id) REFERENCES PARKING_LOT(id) ON DELETE CASCADE
+                ); 
+                ''') 
+    curr.execute('''CREATE TABLE IF NOT EXISTS BOOKING_DETAILS(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                lot_id INTEGER NOT NULL,
+                spot_number TEXT NOT NULL,
+                timestamp_booked DATETIME,
+                timestamp_released DATETIME,
+                vehicle_number TEXT,
+                booking_status TEXT NOT NULL,
+                price INTEGER,
+                FOREIGN KEY(user_id) REFERENCES USERS(id),
+                FOREIGN KEY(lot_id) REFERENCES PARKING_LOT(id),
+                FOREIGN KEY(lot_id, spot_number) REFERENCES PARKING_SPOT(lot_id, spot_number)
+                );
+                ''')
+    conn.commit()
+    conn.close()
+
+
+    #print("Database created succesfully/n")   ----> COULD NOT SEE ANY OUTPUT IN TERMINAL
+    
+
+    
